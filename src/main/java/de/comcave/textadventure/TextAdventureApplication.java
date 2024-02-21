21b831b01abb77086026f6f2bfd6936b996bfc56
Sandro Simperl <sandrosimperl.cc@gmail.com>
2024-02-21 21:33:32 +0100

    init project

---
+++ src/main/java/de/comcave/textadventure/TextAdventureApplication.java
@@ -0,0 +1,13 @@
+package de.comcave.textadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAdventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextAdventureApplication.class, args);
+    }
+
+}
